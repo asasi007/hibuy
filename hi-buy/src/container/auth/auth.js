21f9c8ca4578156bbeefd354f780fa796efaa82b
jjosesuyati <jjose@suyati.com>
2019-10-15 16:18:39 +0530

    jasmin Initial

---
+++ hi-buy/src/container/auth/auth.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Login from '../../components/login/login';
+
+class Auth extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {       
+      };     
+    }
+  
+    render() {
+      return (
+        <Login />
+      );
+    }
+  }
+
+  export default Auth;
